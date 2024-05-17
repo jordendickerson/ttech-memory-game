6b8092142a7687ce27521899e3b2f0b623629d54
Jorden Dickerson <jordendickerson24@gmail.com>
2024-05-16 18:48:31 -0600

    Began work on game

---
+++ script.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log(hasFlippedCard);
+    console.log(firstCard);
+  }
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
